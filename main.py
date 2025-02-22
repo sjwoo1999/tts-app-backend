b576f5014a4c2eec4f0abf017ca727837e3d62fb
_woo_s.j <sjwoo1999@korea.ac.kr>
2025-02-22 23:40:06 +0900

    🔧 Modified: main.py
    Modified: main.py

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 from google.cloud import texttospeech
 import functions_framework
-import os
-from dotenv import load_dotenv
-
-# .env 파일 로드
-load_dotenv()
-
-# 환경 변수 설정
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_CREDENTIALS_PATH")
 
 @functions_framework.http
 def generate_tts(request):
     request_json = request.get_json(silent=True)
     if not request_json or 'ssml' not in request_json:
-        return 'SSML 또는 텍스트를 입력해주세요.', 400
-
+        return 'SSML 필요', 400
+    
     client = texttospeech.TextToSpeechClient()
     synthesis_input = texttospeech.SynthesisInput(ssml=request_json['ssml'])
     voice = texttospeech.VoiceSelectionParams(
@@ -28,4 +20,9 @@ def generate_tts(request):
     response = client.synthesize_speech(
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
-    return response.audio_content, 200, {'Content-Type': 'audio/mp3'}
+    # CORS 헤더 추가
+    headers = {
+        'Content-Type': 'audio/mp3',
+        'Access-Control-Allow-Origin': '*'  # 모든 도메인 허용
+    }
+    return (response.audio_content, 200, headers)
